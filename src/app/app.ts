21399c8b7a1a44932880222140ffc2cf6d762008
Racky17 <dayestray@gmail.com>
2026-03-29 21:51:55 -0300

    creacion del proyecto
    creacion del proyecto que se trabajara en clases usando angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { allIcons } from 'ngx-bootstrap-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('proyecto-clases');
+  protected readonly title = signal('ACME Company Catalog');
 }
