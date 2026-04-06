8f9988a6b466a18d4c4f346c9765fed86284ee3a
Racky17 <dayestray@gmail.com>
2026-04-06 18:21:58 -0400

    Interpolación
    probando con la interpolación

--- src/app/app.ts
+++ src/app/app.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class App {
   protected readonly title = signal('ACME Company Catalog');
+  getTitle() {return this.title();}
   listFilter = signal ("");
   products = signal <IProduct []>([{
     
