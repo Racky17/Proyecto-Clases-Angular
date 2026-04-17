87dd3c4c5d121a7811256cd8398baece768f978c
josemillan2401-code <101983456+Racky17@users.noreply.github.com>
2026-04-17 17:57:32 -0400

    clase 17/04 part 2
    teminando de implementar

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,20 @@
 // Require
-var express= require('express');
+var express = require('express');
+var mysql = require('mysql');
 
 // Inicializar variables
-var app= express();
+var app = express();
+
+// Conexion a la base de datos
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'acme'
+});
+
+//conectar a la base de datos
+conn.connect();
 
 // Ejecutar peticiones
 
@@ -10,6 +22,18 @@ app.listen(3000, ()=>{
     console.log('Express Server - Puerto 3000 Online');
 })
 
+app.get('/productos', (req, res) =>{
+    const sql ='SELECT * FROM productos';
+    conn.query(sql, (err, results) =>{
+        if (err) throw err;
+        res.status(200).json({
+            ok: true,
+            productos: results
+        })
+    })
+})
+
+// Rutas
 app.get('/', (req, res, next) =>{
     res.status(200).json({
         ok: true,
