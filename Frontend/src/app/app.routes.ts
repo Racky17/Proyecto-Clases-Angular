b0547f0a776ef5fab804907448f882f9d7ac7632
Racky17 <101983456+Racky17@users.noreply.github.com>
2026-06-08 09:27:50 -0400

    clase 08-06-2026

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Number } from './features/numbers/components/number/number';
+import { Product } from './product/product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component:welcome, canActivate: [loginGuard] },
+    { path: 'product', component:Product, canActivate: [loginGuard] },
+    { path: 'numbers', component:Number, canActivate: [loginGuard] },
+    { path: 'login', component:login, canActivate: [loginGuard] },
+];
